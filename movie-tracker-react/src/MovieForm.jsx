da929a926eb183d1aeead58470354fef708a2e95
danalm7 <danalazarte7@gmail.com>
2025-06-12 13:15:01 +0200

    mymovietracker

--- movie-tracker-react/src/MovieForm.jsx
+++ movie-tracker-react/src/MovieForm.jsx
@@ -1,35 +1,12 @@
 import { useState } from "react";
+const MovieCard = ({ title, image, rating }) => (
+  <div className="w-32 sm:w-40 md:w-48">
+    <img src={image} alt={title} className="rounded-xl mb-2 shadow-md" />
+    {rating && (
+      <div className="text-white bg-black text-xs px-2 py-1 rounded absolute m-1">{rating}</div>
+    )}
+  </div>
+);
 
-function MovieForm({ addMovie }) {
-  const [name, setName] = useState("");
-  const [year, setYear] = useState("");
+export default MovieCard;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name || !year) return;
-
-    addMovie({ name, year, watched: false });
-    setName("");
-    setYear("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Película"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Año"
-        value={year}
-        onChange={(e) => setYear(e.target.value)}
-      />
-      <button type="submit">Añadir</button>
-    </form>
-  );
-}
-
-export default MovieForm;
